41c436bd776cf5cf957cbb3cdeeb7ce03fd017ec
Usama Butt <108319814+usamabutt79@users.noreply.github.com>
2022-09-30 14:58:46 +0500

    fix error

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-import { BreedsController } from './breeds/breeds.controller';
-import { Breeds } from './breeds';
-
 @Module({
 
   imports: [ProductsModule, MongooseModule.forRoot('mongodb+srv://usamabutt786:18134156-079@cluster0.ajmez0k.mongodb.net/Nest_Practice')],
-  controllers: [AppController, BreedsController],
-  providers: [AppService, Breeds],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
